b37210b4e1770d79871c690990667f4315273351
mismanaged97 <mischiefmanaged97@gmail.com>
2025-05-26 01:00:27 +0530

    Problem of Props drilling

--- ReactRouting/src/App.jsx
+++ ReactRouting/src/App.jsx
@@ -1,28 +1,91 @@
-import { lazy } from 'react'
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+//#region CONTEXT API
+import { useState } from "react"
 
-const Dashboard = lazy(() => import('./Components/Dashboard'))
-const Landing = lazy(() => import('./Components/Landing'))
-
-export default function App() {   // here we can also use suspense syntax as well
+export default function App() {
+  const [count, setCount] = useState(0);
   return <div>
-    <BrowserRouter>
-      <AppBar></AppBar>
-      <Routes>
-        <Route path='/' element={<Landing></Landing>}></Route>
-        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Counter count={count}></Counter>
+    <Buttons setCounter={setCount} count={count}></Buttons>
+  </div >
+}
+
+function Counter({ count }) {
+  return <div>
+    <h1>
+      {count}
+    </h1>
   </div>
 }
 
-function AppBar() {
-  const navigate = useNavigate();
+function Buttons({ setCounter, count }) {
   return <div>
-    <button onClick={() => { navigate("/") }}>Landing Page</button>
-    <button onClick={() => { navigate("/dashboard") }}>Dashboard</button>
+    <button onClick={() => { setCounter(count + 1) }}>Increment</button>
+    <button onClick={() => { setCounter(count - 1) }}>Decrement</button>
   </div>
 }
+//#endregion
+
+
+//#region Props drilling :- You will need to drill props down through the component tree. This gets very hard to maintain and highly verbose makes code highly unreadable
+// Ultimately, passing a props is a great way to explicitly pipe data through your UI tree to the components that use it.
+// But passing props can become verbose and inconvenient when you need to pass some props deeply through the tree, or if many components need the same prop. The nearest common ancestor could be far 
+// removed from components that need data and lifting state up that high can lead to a situation called "PROP DRILLING"
+
+// in the bolow example if we try to move button component inside the Counter component then it becomes necessary to pass the Set Count variable to Counter component which is not necessary for 
+// Counter component as it is mainly uses the Count variable only. So to resolve this problem we have the concept of CONTEXT API
+// import { useState } from "react"
+
+// export default function App() {
+//   const [count, setCount] = useState(0);
+//   return <div>
+//     <Counter count={count}></Counter>
+//     <Buttons setCounter={setCount} count={count}></Buttons>
+//   </div >
+// }
+
+// function Counter({ count }) {
+//   return <div>
+//     <h1>
+//       {count}
+//     </h1>
+//   </div>
+// }
+
+// function Buttons({ setCounter, count }) {
+//   return <div>
+//     <button onClick={() => { setCounter(count + 1) }}>Increment</button>
+//     <button onClick={() => { setCounter(count - 1) }}>Decrement</button>
+//   </div>
+// }
+//#endregion
+
+//#region Lazy loading
+// import { lazy } from 'react'
+// import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+
+// const Dashboard = lazy(() => import('./Components/Dashboard'))
+// const Landing = lazy(() => import('./Components/Landing'))
+
+// export default function App() {   // here we can also use suspense syntax as well
+//   return <div>
+//     <BrowserRouter>
+//       <AppBar></AppBar>
+//       <Routes>
+//         <Route path='/' element={<Landing></Landing>}></Route>
+//         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   </div>
+// }
+
+// function AppBar() {
+//   const navigate = useNavigate();
+//   return <div>
+//     <button onClick={() => { navigate("/") }}>Landing Page</button>
+//     <button onClick={() => { navigate("/dashboard") }}>Dashboard</button>
+//   </div>
+// }
+//#endregion
 
 //#region  Without Lazy Loading
 // import './App.css'
