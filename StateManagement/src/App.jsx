27e26f8acf65e34b0be12cc833daf19432cd0679
mismanaged97 <mischiefmanaged97@gmail.com>
2025-06-02 02:26:40 +0530

    basic understandig of Recoil State management

--- StateManagement/src/App.jsx
+++ StateManagement/src/App.jsx
@@ -1,42 +1,55 @@
 // //#region using RECOIL a state management tool for the more optimization of application
-// import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
-// import './App.css'
-// import { countAtom } from './Store/Atoms/count'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import './App.css'
+import { countAtom, evenSelector } from './Store/Atoms/count'
 
-// export default function App() {
+export default function App() {
+  return (
+    <>
+      <RecoilRoot>
+        <Count></Count>
+      </RecoilRoot>
+    </>
+  )
+}
 
-//   return (
-//     <>
-//       <RecoilRoot>
-//         <Count></Count>
-//       </RecoilRoot>
-//     </>
-//   )
-// }
+function Count() {
+  console.log("Count component re-renders")     // now by using recoil, this component doesn't rerender
+  return <div>
+    <CountRenderer></CountRenderer>
+    <Buttons></Buttons>
+  </div>
+}
 
-// function Count() {
-//   console.log("Count component re-renders")     // now by using recoil, this component doesn't rerender
-//   return <div>
-//     <CountRenderer></CountRenderer>
-//     <Buttons></Buttons>
-//   </div>
-// }
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>
+    <h2>
+      <b> {count} </b>
+    </h2 >
+  </div>
+}
 
-// function CountRenderer() {
-//   const count = useRecoilValue(countAtom)
-//   return <div> <h2>
-//     <b> {count} </b>
-//   </h2 >
-//   </div>
-// }
+function Buttons() {    // by using the another Hook from Recoil will make this component more optimized. As in this component we only need the Set
+  const setCount = useSetRecoilState(countAtom);
+  // const [count, setCount] = useRecoilState(countAtom);    // by using this we have one problem that the buttons will re-render again and again where as we don't really need buttons to re-render
+  return <div>
+    <button onClick={() => setCount(count => count + 1)}>Increment</button>
+    <button onClick={() => setCount(count => count - 1)}>Decrement</button>
+    <br></br>
+    <br></br>
+    <CheckIfEvenOrNot></CheckIfEvenOrNot>
+    {/* <button onClick={() => { setCount(count + 1) }}>Increment</button>
+    <button onClick={() => { setCount(count - 1) }}>Decrement</button> */}
+  </div>
+}
 
-// function Buttons() {
-//   const [count, setCount] = useRecoilState(countAtom);
-//   return <div>
-//     <button onClick={() => { setCount(count + 1) }}>Increment</button>
-//     <button onClick={() => { setCount(count - 1) }}>Decrement</button>
-//   </div>
-// }
+function CheckIfEvenOrNot() {    // this is the ugly way to do this but we have one more option that is RECOIL SELECTOR
+  const count = useRecoilValue(evenSelector);
+  return <div>
+    <b>{count ? null : "It is even"}</b>
+  </div>
+}
 // //#endregion
 
 // // //#region Context API :- It only resolves the problem of props drilling but by using this it automatically re-renders those components as well in which we haven't use Context API
--- StateManagement/src/Store/Atoms/count.jsx
+++ StateManagement/src/Store/Atoms/count.jsx
@@ -1,6 +1,15 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
 export const countAtom = atom({
     key: "countAtom",
     default: 0
 });
+
+export const evenSelector = selector({
+    key: "evenSelector",
+    get: (p) => {
+        const count = p.get(countAtom);
+        console.log(count % 2)
+        return count % 2;
+    }
+})
